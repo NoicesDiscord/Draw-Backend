c4695d85857cdd885cc43a6dc3452cffd38345c7
Ajay Sonune <ajaydiscord155@gmail.com>
2026-08-24 20:11:53 +0530

    fix chat echo

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 
 const app = express();
-app.use(cors()); // This allows your frontend to talk to the backend
+app.use(cors());
 const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*", // We will lock this down to your specific frontend URL later for security
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
@@ -17,29 +17,14 @@ const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log('A player connected:', socket.id);
 
-  // Listen for strokes and instantly broadcast them to everyone else
   socket.on('start', (data) => socket.broadcast.emit('start', data));
   socket.on('draw', (data) => socket.broadcast.emit('draw', data));
   socket.on('stop', () => socket.broadcast.emit('stop'));
 
-  io.on('connection', (socket) => {
-  console.log('A player connected:', socket.id);
-
-  // Existing drawing listeners...
-  socket.on('start', (data) => socket.broadcast.emit('start', data));
-  socket.on('draw', (data) => socket.broadcast.emit('draw', data));
-  socket.on('stop', () => socket.broadcast.emit('stop'));
-
-  // NEW: Chat listener
+  // ONE single chat listener
   socket.on('chat_message', (data) => {
-    // When the server receives a message, broadcast it to EVERYONE in the room (including the sender)
     io.emit('chat_message', data);
   });
-
-  socket.on('disconnect', () => {
-    console.log('Player disconnected:', socket.id);
-  });
-});
 
   socket.on('disconnect', () => {
     console.log('Player disconnected:', socket.id);
