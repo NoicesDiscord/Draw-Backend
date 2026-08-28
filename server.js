0e79838d484950cdce232bde99fead085bac383b
Ajay Sonune <ajaydiscord155@gmail.com>
2026-08-28 17:22:20 +0530

    SERVER STABLE VERSION 1.0

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
-
+ 
 const io = new Server(server, { cors: { origin: "*", methods: ["GET", "POST"] } });
 
 const PUBLIC_MAX_PLAYERS = 8;
